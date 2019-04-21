275e161f6f9dbfebf9300b3394b53160f27145b9
AtilioA <atiliodadalto@hotmail.com>
2019-04-21 18:50:03 -0300

    Testes das novas funções

--- trab1.c
+++ trab1.c
@@ -10,7 +10,6 @@
 
 int main()
 {
-
     tLista lista;
     tCarta carta;
 
@@ -18,7 +17,8 @@ int main()
     printf("Lista vazia criada com sucesso!\n");
 
     printf("Inserindo cartas...\n");
-    carta = criaCarta('1', 'C');
+    // criaListaBaralho(&lista);
+    carta = criaCarta('2', 'C');
     insere(carta, &lista);
     carta = criaCarta('4', 'O');
     insere(carta, &lista);
@@ -34,54 +34,57 @@ int main()
     printf("Imprimindo a lista:\n");
     imprimeLista(&lista);
 
-    printf("\nRetirando carta de valor 7, naipe copas...\n");
-    retira('7', 'C', &lista, &carta);
-    printf("Imprimindo a lista:\n");
-    imprimeLista(&lista);
-    printf("Carta retirada:\n");
-    imprimeCarta(&carta);
+    // printf("Movendo a primeira carta para a terceira posicao...\n");
+    // moveCelula(&lista, lista.primeiro->prox, 3);
 
-    printf("\nRetirando carta de valor 7, naipe espadas...\n");
-    tCarta carta2;
-    retira('7', 'E', &lista, &carta2);
-    printf("Imprimindo a lista:\n");
-    imprimeLista(&lista);
-    printf("Carta retirada:\n");
-    imprimeCarta(&carta2); // ???
+    // printf("Imprimindo a lista:\n");
+    // imprimeLista(&lista);
 
-    printf("\nRecuperando carta de valor J, naipe paus...\n");
-    tCarta carta3;
-    recupera('J', 'P', &lista, &carta3);
+    // printf("Movendo a terceira carta para a primeira posicao...\n");
+    // moveCelula(&lista, lista.primeiro->prox->prox->prox, 1);
+
+    // printf("Imprimindo a lista:\n");
+    // imprimeLista(&lista);
+
+    // printf("Movendo a ultima carta para a primeira posicao...\n");
+    // moveCelula(&lista, lista.ultimo, 1);
+
+    // printf("\nRetirando carta de valor 7, naipe copas...\n");
+    // retira('7', 'C', &lista, &carta);
+    // printf("Imprimindo a lista:\n");
+    // imprimeLista(&lista);
+    // printf("Carta retirada:\n");
+    // imprimeCarta(&carta);
+
+    // printf("\nRetirando carta de valor 7, naipe espadas...\n");
+    // tCarta carta2;
+    // retira('7', 'E', &lista, &carta2);
+    // printf("Imprimindo a lista:\n");
+    // imprimeLista(&lista);
+    // printf("Carta retirada:\n");
+    // imprimeCarta(&carta2);
+
+    // printf("\nRecuperando carta de valor J, naipe paus...\n");
+    // tCarta carta3;
+    // recupera('J', 'P', &lista, &carta3);
+    // printf("Imprimindo a lista:\n");
+    // imprimeLista(&lista);
+    // printf("Carta recuperada:\n");
+    // imprimeCarta(&carta3);
+
+    printf("\nEmbaralhando lista...\n");
+    embaralhaLista(&lista);
     printf("Imprimindo a lista:\n");
     imprimeLista(&lista);
-    printf("Carta retirada:\n");
-    imprimeCarta(&carta3);
+
+    printf("\nEmbaralhando lista...\n");
+    embaralhaLista(&lista);
+    printf("Imprimindo a lista:\n");
+    imprimeLista(&lista);
 
     destroiLista(&lista);
-    printf("Lista destruida com sucesso!\n");
+    printf("\nLista destruida com sucesso!\n");
     imprimeLista(&lista); // Não printa pois a lista está vazia
-
-
-    // tJogador euzinho;
-    // euzinho.mao[0].valor = '2';
-    // euzinho.mao[0].naipe = 'C';
-    // euzinho.mao[1].valor = '9';
-    // euzinho.mao[1].naipe = 'O';
-    // euzinho.mao[2].valor = '3';
-    // euzinho.mao[2].naipe = 'P';
-
-    // tCarta *baralho;
-
-    // baralho = escreveBaralho();
-    // imprimeBaralho(baralho, 40);
-
-    // embaralhaBaralho(baralho);
-    // imprimeBaralho(baralho, 40);
-
-    // imprimeMao(euzinho);
-    // exibeMenu();
-
-    // free(baralho);
 
     return 0;
 }
