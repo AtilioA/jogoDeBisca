3d59aca53b6bf9c31af37c1a9d9908a06547c540
AtilioA <atiliodadalto@hotmail.com>
2019-05-08 23:37:09 -0300

    Esquece esse locale.h

--- trab1.c
+++ trab1.c
@@ -5,13 +5,10 @@
 #include "include/BaralhoEncadeado.h"
 #include "include/PartidaCircular.h"
 #include "include/Jogo.h"
-#include <locale.h>
 
 int main()
 {
-    setlocale(LC_ALL, "Portuguese_Brasil");
     tPartida *partida;
-    printf("teste: ááãçéíóúâêÎôûâÂáÁ\n\n");
     ExibeMenuInicial(partida);
 
     return 0;
